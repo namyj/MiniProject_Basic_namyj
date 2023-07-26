74c1aa272a8f74c27181d4ea267bb15eea3bc87d
namyj <umjiuji61@gmail.com>
2023-07-26 10:48:22 +0900

    feat: spring security 초기 설정

---
+++ src/main/java/com/example/mutsamarket/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.mutsamarket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable) // 테스트를 위해 csrf disable 시킴
+                .authorizeHttpRequests(
+                        authHttp -> authHttp
+                                .requestMatchers("/no-auth").permitAll()
+                                .requestMatchers("/re-auth").authenticated()
+                                .requestMatchers("/").anonymous()
+                );
+
+        return http.build();
+    }
+}
